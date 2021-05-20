35e6dcbdd569a473cc843dfad230a514eb01bcdf
Lindsey Marrero <59493981+aurora733@users.noreply.github.com>
2021-05-20 15:51:29 -0400

    full functionality of StudyList
    Woohoo!!!

--- RosaTEST/StudyList.cs
+++ RosaTEST/StudyList.cs
@@ -1,65 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using FHIRService.Application.Services.FHIR;
+using Hl7.Fhir.Model;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace RosaTEST
 {
     public partial class StudyList : Form
     {
-        public StudyList()
+        public static string PatientID = "siimneela";
+
+        public StudyList([Optional] string[] args)
         {
-            InitializeComponent();
-          
+            if (args != null)
+            {
+                PatientID = args[0];
+            }            
+            InitializeComponent();          
         }
 
         private void StudyList_Load(object sender, EventArgs e)
         {
             PatientInfo_Load();
-            LoadFakeStudyList();
+            LoadStudyList();
         }
 
         private void PatientInfo_Load()
         {
-            lblPatientID.Text = "siimRavi";
-            lblPatientName.Text = "Ravi SIIM";
-            lblPatientDOB.Text = "1992-02-29";
+            FHIRDataService svc = new FHIRDataService();
+            Patient pat = new Patient();
+            pat = svc.GetPatientByID(PatientID);       //TODO: PatientID from ROSA widget
+            lblPatientID.Text = pat.Id;
+            lblPatientName.Text = pat.Name[0].ToString();
+            lblPatientDOB.Text = pat.BirthDate.ToString();
         }
 
 
         public void RenderLoadedData(DataTable dataFromFHIR = null)
         {
-            LoadFakeStudyList();
+            LoadStudyList();
         }
 
-        private void LoadFakeStudyList()
+        private void LoadStudyList()
         {
-                     
-
+            FHIRDataService svc = new FHIRDataService();
             List<string[]> list = new List<string[]>();
-            //list.Add(new string[] { "StudyID", "StudyDescription", "Modality", "StudyDate" });
-            list.Add(new string[] { "a123456789", "XR Chest", "XR", "2021-05-01" });
-            list.Add(new string[] { "b123456789", "US Abdomen", "US", "2021-05-03" });
-            list.Add(new string[] { "c987654321", "NM PET CT", "NM", "2021-05-10" });
+
+            var studies = svc.GetImagingStudiesByPatientID(PatientID);    //TODO: PatientID here too
+
+            foreach (var study in studies)
+            {
+                var lengthSUID = study.Identifier[0].Value.ToString().Length - 8;
+                var StudyUID = study.Identifier[0].Value.ToString().Substring(8, lengthSUID);
+                list.Add(new string[] { study.Id, study.Description, study.Series.FirstOrDefault().Modality.Code.ToString(), study.Started, study.NumberOfInstances.ToString(),
+                       StudyUID });
+                
+            }
 
             DataTable table = ConvertListToDataTable(list);
+
             table.Columns[0].ColumnName = "Study ID";
             table.Columns[1].ColumnName = "Study Description";
             table.Columns[2].ColumnName = "Modality";
             table.Columns[3].ColumnName = "Study Date";
-            //table.Columns[DataGridViewColumn[1]].ColumnName = "Study ID";
+            table.Columns[4].ColumnName = "# Images";
+            
             dataGridView1.DataSource = table;
             
             dataGridView1.Columns[0].MinimumWidth = 125;
             dataGridView1.Columns[1].MinimumWidth = 180;
             dataGridView1.Columns[2].MinimumWidth = 80;
             dataGridView1.Columns[3].MinimumWidth = 125;
+            dataGridView1.Columns[4].MinimumWidth = 5;
+            dataGridView1.Columns[5].Visible = false;
 
         }
 
@@ -91,13 +108,22 @@ namespace RosaTEST
 
         private void button1_Click(object sender, EventArgs e)
         {
-
             string strStudyID;
+
+
+            //study.Series.FirstOrDefault().Uid.ToString();
+
             strStudyID = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString();
-            MessageBox.Show(String.Format("StudyID: {0}", strStudyID));
-            
+            //MessageBox.Show(String.Format("StudyID: {0}", strStudyID));
+
             //launch exam
-            ProcessStartInfo psInfo = new ProcessStartInfo { FileName = "https://hackathon.siim.org/vna/web-viewer/app/viewer.html?series=4dcf5369-ecd612a6-7a3063c8-97930113-7ec3903e", UseShellExecute = true };
+            ProcessStartInfo psInfo = new ProcessStartInfo
+            {
+                FileName = String.Format("https://hackathon.siim.org/vna/stone-webviewer/index.html?study={0}",
+                                            dataGridView1[5, dataGridView1.CurrentCell.RowIndex].Value),
+                UseShellExecute = true
+            };
+
             Process.Start(psInfo);
         }
 
@@ -105,7 +131,7 @@ namespace RosaTEST
         {
             string strPatientID;
             strPatientID = lblPatientID.Text;
-            MessageBox.Show(String.Format("PatientID: {0}", strPatientID));
+            //MessageBox.Show(String.Format("PatientID: {0}", strPatientID));
         }
     }
 }
